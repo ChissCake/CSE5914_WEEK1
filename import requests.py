42383ea2058469c5e7d73320f38f1742b65e89f5
Chris Zhao <czhao6544@gmail.com>
2023-05-10 11:30:24 -0400

    added params passing for request

--- import requests.py
+++ import requests.py
@@ -1,16 +1,17 @@
 import requests
 import sys
 
-url = "https://api.nationalize.io/?name="
+base_uri = "https://api.nationalize.io/?"
 
 while True:
     name = input("Enter a name: ")
-
+    params = {"name": f"{name}"}
     if name == str(1):
         print("quiting...")
         sys.exit(0)
-    r = requests.get(url + name)
+    r = requests.get(base_uri, params=params)
 
     print("Server Response: " + str(r.status_code))
+    
     for i in r.json()["country"]:
         print(i["country_id"] + " : " + str(i["probability"]))
