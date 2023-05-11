c1716689359af5252584ecfce509577d35c856c2
JackFed <jack.fedurek@gmail.com>
2023-05-11 15:12:48 -0400

    Country flag and html

--- import requests.py
+++ import requests.py
@@ -3,6 +3,13 @@ import sys
 
 base_uri = "https://api.nationalize.io/?"
 
+countries_url = "http://www.geognos.com/api/en/countries/flag/"
+
+# Creating an HTML file
+Func = open("output.html","w")
+
+Func.write("<html>\n<head>\n<title> \nOutput Data in an HTML file</title>\n</head>\n")
+
 while True:
     name = input("Enter a name: ")
     params = {"name": f"{name}"}
@@ -13,6 +20,18 @@ while True:
     r = requests.get(base_uri, params=params)
 
     print("Server Response: " + str(r.status_code))
-    
+    Func.write("<p>"+name+"</p>\n")
     for i in r.json()["country"]:
         print(i["country_id"] + " : " + str(i["probability"]))
+        flag = countries_url + i["country_id"] + ".html"
+        flag_img = requests.get(flag)
+
+        #save(flag_img, "png")
+        #print(flag_img.text)
+        #Func.write("<img src=" + flag_img + "/>")
+        Func.write("<p> " + i["country_id"] + " : " + str(i["probability"]) + "</p>\n")
+    
+    # Saving the data into the HTML file
+Func.write("</html>")    
+Func.close()
+#Func.write("<img src=" + flag_img + "/>")
